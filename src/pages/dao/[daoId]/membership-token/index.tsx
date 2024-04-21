d013d2a141239ac72bcb0fb02ed983b8ab0d05b8
kosamit <kosamit@gmail.com>
2024-04-21 21:52:21 +0900

    メンバーシップトークンページの整形

--- src/pages/dao/[daoId]/membership-token/index.tsx
+++ src/pages/dao/[daoId]/membership-token/index.tsx
@@ -28,18 +28,26 @@ const MembershipToken: NextPage = () => {
       ) : (
         <DashboardLayout>
           <div className="h-full lg:px-6">
-            <div className="flex flex-col justify-center w-full py-5 px-4 lg:px-0  max-w-[90rem] mx-auto gap-3">
-              <div className="flex  flex-wrap justify-between">
-                <h3 className="text-center text-xl font-semibold">
-                  メンバーシップトークン
-                </h3>
+            <div className="flex flex-col justify-center w-full py-5 px-4 lg:px-0 max-w-[90rem] mx-auto gap-8">
+              <div className="flex flex-col justify-center w-full mx-auto gap-4">
+                <div className="flex flex-wrap justify-between">
+                  <h3 className="text-center text-xl font-semibold">
+                    メンバーシップトークン一覧
+                  </h3>
+                </div>
+                <div className="w-full flex flex-col gap-4">
+                  <ListMembershipTokens contractAddress={daoId as Address} />
+                </div>
               </div>
-              <div className="flex flex-col gap-4">{data}</div>
-              <div className="w-full flex flex-col gap-4">
-                <CreateMembershipToken contractAddress={data as Address} />
-              </div>
-              <div className="w-full flex flex-col gap-4">
-                <ListMembershipTokens contractAddress={daoId as Address} />
+              <div className="flex flex-col justify-center w-full mx-auto gap-4">
+                <div className="flex flex-wrap justify-between">
+                  <h3 className="text-center text-xl font-semibold">
+                    メンバーシップトークン作成
+                  </h3>
+                </div>
+                <div className="w-full flex flex-col gap-4">
+                  <CreateMembershipToken contractAddress={data as Address} />
+                </div>
               </div>
             </div>
           </div>
