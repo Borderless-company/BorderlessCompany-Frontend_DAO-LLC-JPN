0e973f3670ad7953da1737e0836e1c36884b1f19
kosamit <kosamit@gmail.com>
2024-04-21 21:52:21 +0900

    メンバーシップトークンページの整形

--- src/pages/dao/[daoId]/membership-token/create.tsx
+++ src/pages/dao/[daoId]/membership-token/create.tsx
@@ -5,26 +5,42 @@ import { DashboardLayout } from "@/components/layout/DashboardLayout";
 import { useRouter } from "next/router";
 import { CreateMembershipToken } from "@/components/web3/MembershipTokenFactory/CreateMembershipToken";
 import ListMembershipTokens from "@/components/web3/MembershipTokenFactory/ListMembershipTokens";
+import { GetService } from "@/components/web3/BorderlessCompany/GetService";
+import { useEffect, useState } from "react";
 import { Address } from "viem";
+import { useGetService } from "@/components/hooks/useGetService";
 
 const MembershipTokenCreate: NextPage = () => {
   const router = useRouter();
   const { daoId } = router.query;
+  const [isReady, setIsReady] = useState<boolean>(false);
+  const { data, error, isPending } = useGetService(daoId as Address, 3);
+
+  useEffect(() => {
+    setIsReady(router.isReady);
+  }, [router.isReady]);
+
   return (
-    <DashboardLayout>
-      <div className="h-full lg:px-6">
-        <div className="flex flex-col justify-center w-full py-5 px-4 lg:px-0  max-w-[90rem] mx-auto gap-3">
-          <div className="flex  flex-wrap justify-between">
-            <h3 className="text-center text-xl font-semibold">
-              メンバーシップトークン作成
-            </h3>
+    <>
+      {!isReady || isPending ? (
+        <></>
+      ) : (
+        <DashboardLayout>
+          <div className="h-full lg:px-6">
+            <div className="flex flex-col justify-center w-full py-5 px-4 lg:px-0  max-w-[90rem] mx-auto gap-3">
+              <div className="flex  flex-wrap justify-between">
+                <h3 className="text-center text-xl font-semibold">
+                  メンバーシップトークン作成
+                </h3>
+              </div>
+              <div className="w-full flex flex-col gap-4">
+                <CreateMembershipToken contractAddress={data as Address} />
+              </div>
+            </div>
           </div>
-          <div className="w-full flex flex-col gap-4">
-            <CreateMembershipToken contractAddress={daoId as Address} />
-          </div>
-        </div>
-      </div>
-    </DashboardLayout>
+        </DashboardLayout>
+      )}
+    </>
   );
 };
 
