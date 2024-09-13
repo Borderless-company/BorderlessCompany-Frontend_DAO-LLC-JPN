ffc1897c84f98d777b2930328f04353e085f33d1
Kouki Minamoto <tomatomusicstudio@gmail.com>
2024-09-14 02:13:43 +0900

    change sample data

--- src/pages/estuary/[estId]/index.tsx
+++ src/pages/estuary/[estId]/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { NextPage } from "next";
 import { useParams } from "next/navigation";
 import Image from "next/image";
@@ -6,6 +6,7 @@ import { PiArrowRight } from "react-icons/pi";
 import { Button, Radio, cn } from "@nextui-org/react";
 import { TokenCard } from "@/components/estuary/TokenCard";
 import { RadioGroup } from "@nextui-org/react";
+import { estuarySample } from "@/types";
 
 type EstuaryProps = {
   logoSrc: string;
@@ -21,58 +22,61 @@ const sampleEstuaryProps: EstuaryProps = {
 
 const Estuary: NextPage<EstuaryProps> = () => {
   // const { estId } = useParams<{ estId: string }>();
+  const [selectedTokenId, setSelectedTokenId] = useState<string>("");
   return (
     <div className="w-full h-svh grid place-items-center">
-      <div className="w-full max-w-[35rem] h-fit bg-white rounded-3xl shadow-xl p-6 gap-6 flex flex-col">
+      <div className="w-full max-w-[35rem] h-fit bg-white rounded-3xl shadow-xl p-6 gap-6 flex flex-col border-1 border-slate-200">
         <Image
-          src={sampleEstuaryProps.logoSrc}
+          src={estuarySample.orgLogo}
           alt="DAO LLC Logo"
           width={56}
           height={56}
+          style={{
+            objectFit: "cover",
+            width: "56px",
+            height: "56px",
+          }}
         />
         <div className="flex flex-col gap-2 ">
           <h1 className="text-3xl font-bold text-slate-800">
-            {sampleEstuaryProps.orgName}の社員になる
+            {estuarySample.orgName} に出資する
           </h1>
           <p className="text-slate-800 text-lg font-semibold">
             種類を選択してください。
           </p>
           <RadioGroup
+            value={selectedTokenId}
+            onValueChange={setSelectedTokenId}
             orientation="horizontal"
             classNames={{
               wrapper: cn("flex gap-3 p-2 pb-6 overflow-x-scroll flex-nowrap"),
             }}
           >
-            <TokenCard
-              name="Sample Token"
-              value="1"
-              imageSrc="https://i.seadn.io/s/raw/files/6d531a9a8cd4f6245cc9896a83c9e09b.png?auto=format&dpr=1&w=1000"
-              minPrice={1000}
-              maxPrice={10000}
-            />
-            <TokenCard
-              value="2"
-              name="Sample Token"
-              imageSrc="https://i.seadn.io/s/raw/files/c3f7b3d790c8eff1ce82af91234155ed.png?auto=format&dpr=1&w=1000"
-              minPrice={1000}
-              maxPrice={10000}
-            />
-            <TokenCard
-              value="3"
-              name="Sample Token"
-              imageSrc="https://i.seadn.io/s/raw/files/6d531a9a8cd4f6245cc9896a83c9e09b.png?auto=format&dpr=1&w=1000"
-              minPrice={1000}
-              maxPrice={10000}
-            />
+            {estuarySample.token.map((token) => {
+              return (
+                <TokenCard
+                  name={token.name}
+                  value={token.id}
+                  imageSrc={token.image || ""}
+                  minPrice={token.minPrice || 0}
+                  maxPrice={token.maxPrice || 0}
+                />
+              );
+            })}
           </RadioGroup>
         </div>
         <div className="flex flex-col gap-2">
           <div className="flex justify-between">
             <p className="text-slate-600 font-semibold text-xl">金額</p>
-            <p className="text-slate-800 font-semibold text-xl">¥10,000</p>
+            <p className="text-slate-800 font-semibold text-xl">
+              ¥
+              {estuarySample.token
+                .find((token) => token.id === selectedTokenId)
+                ?.minPrice?.toLocaleString()}
+            </p>
           </div>
           <Button
-            className="w-full h-14 bg-teal-700 text-white text-base font-semibold"
+            className="w-full h-14 bg-yellow-700 text-white text-base font-semibold"
             endContent={<PiArrowRight color="white" />}
           >
             次に進む
