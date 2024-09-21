b60b890aa7a59f061d149a197590736bbff02a71
Kouki Minamoto <tomatomusicstudio@gmail.com>
2024-09-22 02:51:35 +0900

    add connect wallet

--- src/pages/estuary/[estId]/index.tsx
+++ src/pages/estuary/[estId]/index.tsx
@@ -1,12 +1,6 @@
-import { useState } from "react";
 import { NextPage } from "next";
-import { TokenSelection } from "@/components/estuary/TokenSelection";
-import { useAtom } from "jotai";
-import { estuaryPageAtom } from "@/atoms";
-import KYCPage from "@/components/estuary/KYCPage";
-import KYCSucceededPage from "@/components/estuary/KYCSucceededPage";
-import ReceivedPage from "@/components/estuary/ReceivedPage";
-import AgreementPage from "@/components/estuary/AgreementPage";
+import { EstuaryContainer } from "../_components/EstuaryContainer";
+import { ThirdwebProvider } from "thirdweb/react";
 
 type EstuaryProps = {
   logoSrc: string;
@@ -16,24 +10,10 @@ type EstuaryProps = {
 
 const Estuary: NextPage<EstuaryProps> = () => {
   // const { estId } = useParams<{ estId: string }>();
-  const [estuaryPage] = useAtom(estuaryPageAtom);
-
   return (
-    <div className="w-full h-svh grid place-items-center">
-      <div className="w-full max-w-[35rem] h-[720px] bg-stone-50 rounded-3xl shadow-xl flex flex-col border-1 border-slate-200">
-        {estuaryPage === 0 ? (
-          <TokenSelection />
-        ) : estuaryPage === 1 ? (
-          <KYCPage />
-        ) : estuaryPage === 2 ? (
-          <KYCSucceededPage />
-        ) : estuaryPage === 3 ? (
-          <AgreementPage />
-        ) : estuaryPage === 4 ? (
-          <ReceivedPage />
-        ) : null}
-      </div>
-    </div>
+    <ThirdwebProvider>
+      <EstuaryContainer />
+    </ThirdwebProvider>
   );
 };
 
---
+++ src/pages/estuary/_components/EstuaryContainer.tsx
@@ -0,0 +1,36 @@
+import { estuaryPageAtom } from "@/atoms";
+import AgreementPage from "@/components/estuary/AgreementPage";
+import KYCPage from "@/components/estuary/KYCPage";
+import KYCSucceededPage from "@/components/estuary/KYCSucceededPage";
+import ReceivedPage from "@/components/estuary/ReceivedPage";
+import { TokenSelection } from "@/components/estuary/TokenSelection";
+import { useAtom } from "jotai";
+import { FC } from "react";
+import { client } from "@/utils/client";
+import { ConnectButton } from "@/components/estuary/ConnectButton";
+import { ConnectButton as TWConnectButton } from "thirdweb/react";
+
+export const EstuaryContainer: FC = () => {
+  const [estuaryPage] = useAtom(estuaryPageAtom);
+  return (
+    <div className="w-full h-svh grid place-items-center">
+      <div className="relative w-full max-w-[35rem] h-[720px] bg-stone-50 rounded-3xl shadow-xl flex flex-col border-1 border-slate-200">
+        <div className="absolute top-3 right-3">
+          <ConnectButton />
+          {/* <TWConnectButton client={client} /> */}
+        </div>
+        {estuaryPage === 0 ? (
+          <TokenSelection />
+        ) : estuaryPage === 1 ? (
+          <KYCPage />
+        ) : estuaryPage === 2 ? (
+          <KYCSucceededPage />
+        ) : estuaryPage === 3 ? (
+          <AgreementPage />
+        ) : estuaryPage === 4 ? (
+          <ReceivedPage />
+        ) : null}
+      </div>
+    </div>
+  );
+};
